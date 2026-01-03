d13c77a5dfc3abf83ccbd56a218d5cb41a8a095c
Yura Popovich <upopovic291@gmail.com>
2026-01-03 22:07:45 +0200

    Add minification option to Vite build config

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-	base:
-		process.env.NODE_ENV === 'production'
-			? '/Additive-Scrambler-Visualizer/'
-			: '/',
+export default defineConfig(({ mode }) => ({
+	base: mode === 'production' ? '/Additive-Scrambler-Visualizer/' : '/',
 	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './src/test/setup.ts',
-	},
+
 	build: {
 		sourcemap: false,
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true,
-			},
-		},
+		minify: 'esbuild',
 		rollupOptions: {
 			output: {
 				manualChunks: {
@@ -37,4 +24,9 @@ export default defineConfig({
 			},
 		},
 	},
-});
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './src/test/setup.ts',
+	},
+}));
